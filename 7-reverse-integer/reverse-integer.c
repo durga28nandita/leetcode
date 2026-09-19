f1eb5d062282c947a642d922adb30f7d9f29703d
durga28nandita <durga28.nandita@gmail.com>
2026-09-19 20:40:17 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (49.29%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.c
@@ -0,0 +1,13 @@
+long long int reverse(int x){
+    long long rev=0;
+    while(x!=0)
+    {
+        if(rev>INT_MAX/10 || rev< INT_MIN/10)
+        {
+            return 0;
+        }
+        rev=rev*10+x%10;
+        x/=10;
+    }
+    return rev;
+}
