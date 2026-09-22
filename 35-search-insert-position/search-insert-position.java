9b2fcc93f88b9afbb5262091053f74dee1ff7d76
durga28nandita <durga28.nandita@gmail.com>
2026-09-22 08:48:34 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (66.78%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int l=0;
+        int r=nums.length-1;
+        int mid=0;
+        while(l<=r)
+        {
+            mid=(l+r)/2;
+            if(target==nums[mid])
+            {
+                return mid;
+            }
+            else if(target>nums[mid])
+            {
+                l=mid+1;
+            }
+            else
+            {
+                r=mid-1;
+            }
+        }
+        return l;
+        
+    }
+}
