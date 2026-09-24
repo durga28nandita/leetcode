a8d7dcb811172e54ba425bda79767553f20270e7
durga28nandita <durga28.nandita@gmail.com>
2026-09-24 11:10:43 +0530

    Time: 9 ms (96.05%) | Memory: 47.7 MB (44.40%) - LeetSync

---
+++ 1508-longest-happy-prefix/longest-happy-prefix.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String longestPrefix(String s) {
+        int n=s.length();
+        int []lps=new  int[n];
+        int i=1;
+        int len=0;
+        while(i<n)
+        {
+            if(s.charAt(i)==s.charAt(len))
+            {
+                len++;
+                lps[i]=len;
+                i++;
+            }
+            else if(len>0)
+            {
+                len=lps[len-1];
+            }
+            else
+            {
+                i++;
+            }
+        }
+        return s.substring(0,lps[n-1]);
+
+
+    }
+
+}
